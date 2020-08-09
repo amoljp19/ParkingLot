f47c2f7ef2c57a5126959a31db4a2eeca595377d
amol pawar <amoljp19@gmail.com>
2020-08-09 09:29:44 +0530

    added all commands as a enum class

---
+++ src/main/kotlin/Commands.kt
@@ -0,0 +1,37 @@
+enum class Commands(val commandName: String) {
+
+    CREATE("create_parking_lot"),
+    PARK("park"),
+    LEAVE("leave"),
+    STATUS("status");
+
+    fun equalsName(otherCommandName: String): Boolean {
+        return commandName == otherCommandName
+    }
+
+    override fun toString(): String {
+        return commandName
+    }
+
+    companion object {
+        fun findByCommandName(cmndName: String): Commands? {
+            for (command in values()) {
+                if (command.toString() == cmndName) {
+                    return command
+                }
+            }
+            return null
+        }
+
+        fun <T : Enum<T>?> getEnumFromString(c: Class<T>?, string: String?): T? {
+            if (c != null && string != null) {
+                try {
+                    return java.lang.Enum.valueOf(c, string.trim { it <= ' ' }.toUpperCase())
+                } catch (ex: IllegalArgumentException) {
+                }
+            }
+            return null
+        }
+    }
+
+}
