9f1fa8e589d5a2e4b60a60441ba9d12b10211ff0
amol pawar <amoljp19@gmail.com>
2020-08-09 20:00:11 +0530

    unit test case for incorrect command done

---
+++ src/test/kotlin/commands/CommandsTest.kt
@@ -0,0 +1,17 @@
+package commands
+
+import javafx.scene.web.HTMLEditorSkin.Command
+import junit.framework.Assert.assertTrue
+import org.junit.Test
+
+
+class CommandsTest {
+
+    @Test
+    @Throws(Exception::class)
+    fun testInCorrectCommand() {
+        val command: Commands? = Commands.findByCommandName("create_parking_lot_for_truck")
+        assertTrue(command == null)
+    }
+
+}
