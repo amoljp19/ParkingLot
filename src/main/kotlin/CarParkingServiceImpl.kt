60ecb53953425727b4b50be34aa3a5ee0a0e0707
amol pawar <amoljp19@gmail.com>
2020-08-09 09:58:57 +0530

    Added CarParkingServiceImpl.kt, methods not yet implemented

---
+++ src/main/kotlin/CarParkingServiceImpl.kt
@@ -0,0 +1,18 @@
+class CarParkingServiceImpl : CarParkingService {
+
+    override fun createParkingLot(capacity: String) {
+        TODO("Not yet implemented")
+    }
+
+    override fun parkCar(regNo: String, color: String) {
+        TODO("Not yet implemented")
+    }
+
+    override fun leaveCar(slotNo: String) {
+        TODO("Not yet implemented")
+    }
+
+    override fun carParkingStatus() {
+        TODO("Not yet implemented")
+    }
+}
