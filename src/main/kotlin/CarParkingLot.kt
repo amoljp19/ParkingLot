fcac089cbf737bd47330f55946ec7f0ee32f0dd5
amol pawar <amoljp19@gmail.com>
2020-08-09 10:22:11 +0530

    added other methods and implemenation

--- src/main/kotlin/CarParkingLot.kt
+++ src/main/kotlin/CarParkingLot.kt
@@ -4,4 +4,20 @@ object CarParkingLot {
     fun initCarParkingService(){
         carParkingService = CarParkingServiceImpl()
     }
+
+    fun createParkingLot(capacity:String){
+        carParkingService.createParkingLot(capacity)
+    }
+
+    fun parkCar(regNo: String, color: String){
+        carParkingService.parkCar(regNo, color)
+    }
+
+    fun leaveCar(slotNo: String){
+        carParkingService.leaveCar(slotNo)
+    }
+
+    fun carParkingStatus(){
+        carParkingService.carParkingStatus()
+    }
 }
