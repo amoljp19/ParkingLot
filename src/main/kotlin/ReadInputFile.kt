c008c9d56b776f5d2054b04b75498a1db3c5c335
amol pawar <amoljp19@gmail.com>
2020-08-10 18:37:19 +0530

    created setup and parking_lot script files for automation, also changed main() to passing file name as argument

--- src/main/kotlin/ReadInputFile.kt
+++ src/main/kotlin/ReadInputFile.kt
@@ -3,8 +3,12 @@ import java.io.File
 import java.io.InputStream
 
 
-fun main() {
-    val inputStream: InputStream = File(System.getProperty("user.dir")+"/file_input.txt").inputStream()
+fun main(args: Array<String>) {
+    if (args.size == 0) {
+        println("Please provide a name as a command-line argument")
+        return
+    }
+    val inputStream: InputStream = File(System.getProperty("user.dir")+"/${args[0]}").inputStream()
     readInputFileText(inputStream)
 }
 
